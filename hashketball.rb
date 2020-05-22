b480e998d7a4af995e7365c41d7454e46c9ee8b6
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-22 17:20:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,27 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => ""
+      :colors => []
+      :players => []
+    }
+    :away => {
+      :team_name => ""
+      :colors => []
+      :players => [
+        {
+          :player_name => 
+          :number =>
+          :shoe =>
+          :points =>
+          :rebounds =>
+          :assists =>
+          :steals =>
+          :blocks =>
+          :slam_dunks =>
+        }
+        ]
+    }
+  }
+end
